245f64acabc2825b04bcd4ab516eab48b80f15ec
DarkSmile92 <robin.kretzschmar@yahoo.de>
2019-07-24 15:07:57 +0200

    Updates

---
+++ precache-manifest.927383cb4fbd0864bc2bfb8bbd737459.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d66137ebc2fc2974c535d100b12365",
+    "url": "/rik/index.html"
+  },
+  {
+    "revision": "58ac46ff4e21bc61e7af",
+    "url": "/rik/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "350b18755f7ec57255ed",
+    "url": "/rik/static/js/2.9f986fd9.chunk.js"
+  },
+  {
+    "revision": "58ac46ff4e21bc61e7af",
+    "url": "/rik/static/js/main.46baa806.chunk.js"
+  },
+  {
+    "revision": "0d03e31bc80601d91856",
+    "url": "/rik/static/js/runtime~main.90be9e62.js"
+  }
+]);
